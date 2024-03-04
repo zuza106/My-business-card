d78e0936cd73bccd405bee844aa0720410eaff23
Zuzanna Skorupka <zuza106@gmail.com>
2024-03-04 07:52:50 +0100

    refactoring js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,48 @@
-console.log("Cześć!");
-
-let buttonDarkMode = document.querySelector(".js-buttonDarkMode");
-let darkTheme = document.querySelector(".body--darkTheme");
-let body = document.querySelector(".body");
-let image = document.querySelector(".main__image");
-let removePhoto = document.querySelector(".js-removePhoto");
-let removeButton = document.querySelector(".js-removeButton");
-let changeText = document.querySelector(".js-changeText");
-let main__captions = document.querySelector(".main__captions");
-let brightMain__captions = document.querySelector(".main__captions--bright");
+{
+    const welcome = () => { console.log("Cześć!") };
 
 
 
-buttonDarkMode.addEventListener("click", () => {
-    body.classList.toggle("body--darkTheme");
-    changeText.innerText = body.classList.contains("body--darkTheme") ? "jasny ☀️" : "ciemny 🌙";
+    const changeCaptionsColor = (changeDarkMode) => {
+        let main__captions = document.querySelector(".main__captions");
+        let brightMain__captions = document.querySelector(".main__captions--bright");
 
-    if (body.classList.contains("body--darkTheme")) {
-        main__captions.classList.toggle = ("main__captions--bright")
-    } else {
-        main__captions.classList.toggle = ("main__captions")
+        if (body.classList.contains("body--darkTheme")) {
+            main__captions.classList.toggle = ("main__captions--bright")
+        } else {
+            main__captions.classList.toggle = ("main__captions")
+        }
     }
-});
 
 
+    const changeDarkMode = () => {
+        let darkTheme = document.querySelector(".body--darkTheme");
+        let body = document.querySelector(".body");
+        let changeText = document.querySelector(".js-changeText");
 
-removePhoto.addEventListener("click", () => {
-    image.remove();
-    removeButton.remove();
-});
+        body.classList.toggle("body--darkTheme");
+        changeText.innerText = body.classList.contains("body--darkTheme") ? "jasny ☀️" : "ciemny 🌙";
+    }
 
 
+    const removeImage = () => {
+        const image = document.querySelector(".main__image");
+        const removeButton = document.querySelector(".js-removeButton");
+        image.remove();
+        removeButton.remove();
+
+    }
+
+    const init = () => {
+        const buttonDarkMode = document.querySelector(".js-buttonDarkMode");
+        buttonDarkMode.addEventListener("click", changeDarkMode);
+
+
+        let removePhoto = document.querySelector(".js-removePhoto");
+        removePhoto.addEventListener("click", removeImage);
+
+    };
+
+    init();
+
+}
